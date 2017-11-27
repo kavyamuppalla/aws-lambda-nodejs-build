6ace88ea84c3c9eaa17554df16fb145b59a420f2
[Kavya Muppalla] <[kavyamuppalla@gmail.com]>
2017-11-26 19:17:18 -0500

    Modified for adding build code

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-var child_process = require('child_process');
+exports.handler = (event, context, callback) => {
+	var AWS = require('aws-sdk');
+	var clone = require('nodegit-clone');
+	var fs = require('fs');
+	var child_process = require('child_process');
 
-child_process.exec('cd /projects/node-js-sample/ & npm install & tar cvf node-js-sample.tar .', function(error, stdout, stderr) {
-	console.log(`stdout: ${stdout}`);
-  	console.log(`stderr: ${stderr}`);
+	AWS.config.update({ region: 'us-east-1' });
+	var s3 = new AWS.S3({params: {Bucket: 'serverless-pipeline-artifacts'}, region: 'us-east-1'});
 
-  	//Move Artifact to S3
-});
+	clone({url: 'https://github.com/kavyamuppalla/sample-node', localPath: '/tmp/sample-node'}).then(function(repo) {
+		console.log("Path ==== " + repo.path());
+		child_process.exec('cd /tmp/sample-node/ & npm install & zip -r sample-node.zip .', function(error, stdout, stderr) {
+			console.log(`stdout: ${stdout}`);
+		  	console.log(`stderr: ${stderr}`);
+
+		  	//Move Artifact to S3
+		  	fs.readFile('/tmp/sample-node/sample-node.zip', function(err, data) {
+		  		console.log("Error ==== " + err);
+		  		s3.putObject({
+			  		Bucket: 'serverless-pipeline-artifacts',
+			  		Key: 'artifacts/sample-node.zip',
+			  		Body: data
+			  	}, function(err, data) {
+			  		console.log("Error ==== " + err);
+			  		console.log("Data ==== " + data);
+			  	});
+		  	});
+
+		  	fs.readFile('/tmp/sample-node/infrastructure/resource.json', function(err, data) {
+		  		console.log("Error ==== " + err);
+		  		s3.putObject({
+			  		Bucket: 'serverless-pipeline-artifacts',
+			  		Key: 'templates/sample-node/resource.json',
+			  		Body: data
+			  	}, function(err, data) {
+			  		console.log("Error ==== " + err);
+			  		console.log("Data ==== " + data);
+			  	});
+		  	});
+		});
+	});
+	callback(null, "SUCCESS");
+};
