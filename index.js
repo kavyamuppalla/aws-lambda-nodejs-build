c87ecccf497a8fe5ec99baecac16a1d341061526
[Kavya Muppalla] <[kavyamuppalla@gmail.com]>
2017-11-27 20:38:53 -0500

    Modified for fixing the Certificate issue

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 exports.handler = (event, context, callback) => {
 	var AWS = require('aws-sdk');
-	var clone = require('nodegit-clone');
+	var git = require('nodegit');
 	var fs = require('fs');
 	var child_process = require('child_process');
 
 	AWS.config.update({ region: 'us-east-1' });
 	var s3 = new AWS.S3({params: {Bucket: 'serverless-pipeline-artifacts'}, region: 'us-east-1'});
+	var cloneOptions = {};
+	cloneOptions.fetchOpts = {
+		callbacks: {
+			certificateCheck: function() { return 1; }
+		}
+	};
 
-	clone({url: 'https://github.com/kavyamuppalla/sample-node', localPath: '/tmp/sample-node'}).then(function(repo) {
-		console.log("Path ==== " + repo.path());
-		child_process.exec('cd /tmp/sample-node/ & npm install & zip -r sample-node.zip .', function(error, stdout, stderr) {
-			console.log(`stdout: ${stdout}`);
+	console.log("Before GitHub");
+	child_process.exec('rm -Rf /tmp/sample-node', function(error, stdout, stderr) {
+		console.log(`stdout: ${stdout}`);
+		console.log(`stderr: ${stderr}`);
+		git.Clone('https://github.com/kavyamuppalla/sample-node', '/tmp/sample-node', cloneOptions).then(function(repo) {
+			console.log("Path ==== " + repo.path());
+			child_process.exec('cd /tmp/sample-node/ & npm install & zip -r sample-node.zip .', function(error, stdout, stderr) {
+				console.log(`stdout: ${stdout}`);
 		  	console.log(`stderr: ${stderr}`);
 
 		  	//Move Artifact to S3
@@ -37,7 +47,11 @@ exports.handler = (event, context, callback) => {
 			  		console.log("Data ==== " + data);
 			  	});
 		  	});
+			});
+		}).catch(function(err) {
+			console.error(err);
 		});
 	});
+	
 	callback(null, "SUCCESS");
 };
