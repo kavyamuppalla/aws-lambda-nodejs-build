49ed6b4b75a7691e766b7277c2009678a18ed15a
[Kavya Muppalla] <[kavyamuppalla@gmail.com]>
2017-11-28 23:10:22 -0500

    Modified for AWS Lambda

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
+require('lambda-git')({'targetDirectory': '/tmp/pipeline/git'});
+const execSync = require('child_process').execSync;
+const AWS = require('aws-sdk');
+const fs = require('fs');
+exports.GIT_DIR = '/tmp/pipeline/git';
+exports.CLONE_DIR = '/tmp/pipeline/clone';
+exports.GIT_TEMPLATE_DIR = '/tmp/pipeline/git/usr/share/git-core/templates';
+exports.GIT_EXEC_DIR = '/tmp/pipeline/git/usr/libexec/git-core';
+
 exports.handler = (event, context, callback) => {
-	var AWS = require('aws-sdk');
-	var git = require('nodegit');
-	var fs = require('fs');
-	var child_process = require('child_process');
+
+	console.log("Executing Shell Commands");
+	execSync(`
+		mkdir -p ${exports.CLONE_DIR}
+		export GIT_TEMPLATE_DIR=${exports.GIT_TEMPLATE_DIR}
+		export GIT_EXEC_PATH=${exports.GIT_EXEC_DIR}
+
+		echo $GIT_TEMPLATE_DIR
+		echo $GIT_EXEC_PATH
+		export PATH=$PATH:$GIT_TEMPLATE_DIR:$GIT_EXEC_PATH
+		echo $PATH
+
+		git --version
+		cd ${exports.CLONE_DIR}
+		git clone https://github.com/kavyamuppalla/sample-node
+		cd sample-node
+		
+		npm install
+		zip -r sample-node.zip .
+	`);
 
 	AWS.config.update({ region: 'us-east-1' });
 	var s3 = new AWS.S3({params: {Bucket: 'serverless-pipeline-artifacts'}, region: 'us-east-1'});
-	var cloneOptions = {};
-	cloneOptions.fetchOpts = {
-		callbacks: {
-			certificateCheck: function() { return 1; }
-		}
-	};
+	//Move Artifact to S3
+	fs.readFile('/tmp/sample-node/sample-node.zip', function(err, data) {
+		console.log("Error ==== " + err);
+		s3.putObject({
+  		Bucket: 'serverless-pipeline-artifacts',
+  		Key: 'artifacts/sample-node.zip',
+  		Body: data
+  	}, function(err, data) {
+  		console.log("Error ==== " + err);
+  		console.log("Data ==== " + data);
+  	});
+	});
 
-	console.log("Before GitHub");
-	child_process.exec('rm -Rf /tmp/sample-node', function(error, stdout, stderr) {
-		console.log(`stdout: ${stdout}`);
-		console.log(`stderr: ${stderr}`);
-		git.Clone('https://github.com/kavyamuppalla/sample-node', '/tmp/sample-node', cloneOptions).then(function(repo) {
-			console.log("Path ==== " + repo.path());
-			child_process.exec('cd /tmp/sample-node/ & npm install & zip -r sample-node.zip .', function(error, stdout, stderr) {
-				console.log(`stdout: ${stdout}`);
-		  	console.log(`stderr: ${stderr}`);
-
-		  	//Move Artifact to S3
-		  	fs.readFile('/tmp/sample-node/sample-node.zip', function(err, data) {
-		  		console.log("Error ==== " + err);
-		  		s3.putObject({
-			  		Bucket: 'serverless-pipeline-artifacts',
-			  		Key: 'artifacts/sample-node.zip',
-			  		Body: data
-			  	}, function(err, data) {
-			  		console.log("Error ==== " + err);
-			  		console.log("Data ==== " + data);
-			  	});
-		  	});
-
-		  	fs.readFile('/tmp/sample-node/infrastructure/resource.json', function(err, data) {
-		  		console.log("Error ==== " + err);
-		  		s3.putObject({
-			  		Bucket: 'serverless-pipeline-artifacts',
-			  		Key: 'templates/sample-node/resource.json',
-			  		Body: data
-			  	}, function(err, data) {
-			  		console.log("Error ==== " + err);
-			  		console.log("Data ==== " + data);
-			  	});
-		  	});
-			});
-		}).catch(function(err) {
-			console.error(err);
-		});
+	//Move Infrastructure Template to S3
+	fs.readFile('/tmp/sample-node/infrastructure/resource.json', function(err, data) {
+		console.log("Error ==== " + err);
+		s3.putObject({
+  		Bucket: 'serverless-pipeline-artifacts',
+  		Key: 'templates/sample-node/resource.json',
+  		Body: data
+  	}, function(err, data) {
+  		console.log("Error ==== " + err);
+  		console.log("Data ==== " + data);
+  	});
 	});
 	
 	callback(null, "SUCCESS");
